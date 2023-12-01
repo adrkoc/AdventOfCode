8ad4e90ed55b34ea90495fbc500aa0277605096c
adrián kocifaj <125208707+adrkoc@users.noreply.github.com>
2023-12-01 10:59:32 +0100

    day01

---
+++ day01.kt
@@ -0,0 +1,60 @@
+import java.io.File
+import java.io.InputStream
+
+fun firstStar(file: String): Int {
+    val input: InputStream = File(file).inputStream()
+    val lines: MutableList<String> = mutableListOf()
+    input.bufferedReader().forEachLine { line -> lines.add(line) }
+
+    var result: Int = 0
+    for (line in lines) {
+        val digits: String = line.filter { it.isDigit() }
+        if (digits.isNotEmpty()) {
+            result += (digits[0].digitToInt() * 10 + digits[digits.length - 1].digitToInt())
+        }
+    }
+    return result
+}
+
+fun secondStar(file: String): Int {
+    val input: InputStream = File(file).inputStream()
+    val lines: MutableList<String> = mutableListOf()
+    input.bufferedReader().forEachLine { line -> lines.add(line) }
+
+    val correctedLines: MutableList<String> = mutableListOf()
+    for (line in lines) {
+        correctedLines.add(wordsToDigits(line))
+    }
+
+    var result: Int = 0
+    for (line in correctedLines) {
+        val digits: String = line.filter { it.isDigit() }
+        if (digits.isNotEmpty()) {
+            result += (digits[0].digitToInt() * 10 + digits[digits.length - 1].digitToInt())
+        }
+    }
+    return result
+}
+
+fun wordsToDigits(string: String): String {
+    val numbers: MutableList<Pair<String, String>> = mutableListOf(
+        Pair("one", "1e"), Pair("two", "2o"), Pair("three", "3e"),
+        Pair("four", "4r"), Pair("five", "5e"), Pair("six", "6x"),
+        Pair("seven", "7n"), Pair("eight", "8t"), Pair("nine", "9e"),
+    )
+
+    var result: String = ""
+    for (character in string) {
+        result += character
+        for (number in numbers) {
+            result = result.replace(number.first, number.second)
+        }
+    }
+    return result
+}
+
+
+fun main(args: Array<String>) {
+    println(firstStar("src/main/kotlin/day01_input01.txt")) // 54644
+    println(secondStar("src/main/kotlin/day01_input01.txt")) // 53348
+}
